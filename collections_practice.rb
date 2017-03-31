b93513e18e0b05ac1fb5330a40d50e9ae49f92e9
Quinton Boltin <quinton@valleyexpetec.com>
2017-03-31 05:22:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  tmp = array.pop
+  array.insert(1, tmp)
+end
+
+def swap_elements_from_to(array, i, di)
+  tmp = array.delete(array[di])
+  array.insert(di, array[i])
+  array[i] = tmp
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  rarr = []
+  array.each do |i|
+    i[2] = "$"
+    rarr << i
+  end
+end
+
+def find_a(array)
+  array.select {|i| i.upcase.start_with?("A")}
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect { |e, i| i == 1 ? e : e << "s" }
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4.1
+  describe 'swap_elements_from_to' do
+    it 'swaps a chosen indice with another' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],0,2)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
